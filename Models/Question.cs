bbbb31a6b02797068a185d1f81e4d32ff2514fd8
Vladislav Mekhonoshin <vladmexon@gmail.com>
2022-06-25 19:30:43 +0500

    Database updated

---
+++ Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamification.Models
+{
+    public class Question
+    {
+        [Key]
+        public Guid QuestionId { get; set; }
+        public int QuestionNumber { get; set; }
+        public string QuestionText { get; set; }
+        public Quiz Quiz { get; set; }
+    }
+}
---
+++ Models/Round.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamification.Models
+{
+    public class Round
+    {
+        [Key]
+        public Guid RoundId { get; set; }
+        public Team Team { get; set; }
+        public Quiz Quiz { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
