db06334816be0d73db7f48dbed5bd1f95d8879aa
Vladislav Mekhonoshin <vladmexon@gmail.com>
2022-06-27 14:58:36 +0500

    added repositories for new tables

---
+++ Data/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gamification.Models;
+
+namespace Gamification.Data.Interfaces
+{
+    interface IQuestionRepository
+    {
+        Task<Guid> Create(Question newQuestion);
+        Task<Question> GetQuestionById(Guid quizId);
+        Task<List<Question>> GetAllQuestionsByQuiz(Quiz quiz);
+    }
+}
---
+++ Data/RoundRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gamification.Data.Interfaces;
+using Gamification.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamification.Data
+{
+    public class RoundRepository:IRoundRepository
+    {
+        private readonly ApplicationContext db;
+        public RoundRepository(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<Guid> Create(Round newRound)
+        {
+            db.Rounds.Add(newRound);
+            await db.SaveChangesAsync();
+            return newRound.RoundId;
+        }
+        public async Task<Round> GetRoundById(Guid roundId)
+        {
+            return await db.Rounds.FirstOrDefaultAsync(r => r.RoundId == roundId);
+        }
+        public async Task<List<Round>> GetAllRounds()
+        {
+            return await db.Rounds.ToListAsync();
+        }
+    }
+}
