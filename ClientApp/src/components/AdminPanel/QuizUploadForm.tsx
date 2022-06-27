af6729241355f311f7a8587eedb97f56d8d42396
arrKhange1 <therock65frostik@gmail.com>
2022-06-27 12:24:55 +0500

    custom text input added

--- ClientApp/src/components/AdminPanel/QuizUploadForm.tsx
+++ ClientApp/src/components/AdminPanel/QuizUploadForm.tsx
@@ -1,27 +1,45 @@
 import React, { useState } from 'react';
 import { CustomButton } from '../shared/components/UI/CustomButton/CustomButton';
 import IQuiz from '../../@types/AdminPanel/IQuiz';
+import CustomTextInput from '../shared/components/UI/CustomTextInput/CustomTextInput';
 
 function QuizUploadForm({addQuiz}:any) {
 
     const [quiz, setQuiz] = useState<IQuiz>({name:'', 
         dateBegin:'', dateEnd:''})
-
+    
     const submitAdd = (e:any) => {
         e.preventDefault();
         addQuiz(quiz);
 
-        /* TODO: setQuiz({name:'', 
-        dateBegin:'', dateEnd:'', file:''}) */
+        setQuiz({name:'', dateBegin:'', dateEnd:''});
     }
 
     return (
-        <form action="" style={{display:'flex', 
-            flexDirection:'column'}}>
-            <input type="text" placeholder='Название'/> {/* TODO: custom text input
+        <form encType="multipart/form-data" action="" style={{display:'flex', 
+            flexDirection:'column', alignItems:'center'}}>
+            <CustomTextInput
+            type="text"
+            placeholder='Название'
+            value={quiz.name}
+            onChange={(e:any) => setQuiz({...quiz, name: e.target.value})}
+            /> {/* TODO: custom text input
             + custom file input + fill quiz */}
-            <input type="text" placeholder='Дата начала'/>
-            <input type="text" placeholder='Дата окончания'/>
+            <CustomTextInput
+            type="text"
+            placeholder='Дата начала'
+            value={quiz.dateBegin}
+            onChange={(e:any) => setQuiz({...quiz,
+                dateBegin: e.target.value})}
+            />
+            <CustomTextInput
+            type="text"
+            placeholder='Дата окончания'
+            value={quiz.dateEnd}
+            onChange={(e:any) => setQuiz({...quiz,
+                 dateEnd: e.target.value})}
+              />
+
             <CustomButton onClick={submitAdd}>Добавить</CustomButton>
          </form>
     );
